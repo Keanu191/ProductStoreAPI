3acb187354466bee3d8e38a63c5c74e13da68ed7
KEANU FARRO <30074191@tafe.wa.edu.au>
2024-08-14 10:55:07 +0800

    Done sessions 5 work

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationDemoS4.Models;
+
+namespace WebApplicationDemoS4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _shopContext;
+
+        public ProductsController(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+            _shopContext.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllProducts()
+        {
+            var products = await _shopContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet, Route("get")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _shopContext.Products.FindAsync(id);
+            // here we use Ok product
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [HttpPost]
+
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _shopContext.Products.Add(product);
+            await _shopContext.SaveChangesAsync();
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+        /*
+        [HttpGet]
+
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+        */
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,25 +1,33 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace WebApplicationDemoS4.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
 
-        public string? UnitName { get; set; }
+        public string? UnitName { get; set; } = string.Empty;
 
-        public string Name { get; set; }
+        [Required]
 
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+
+        public string Description { get; set; } = string.Empty;
 
         public decimal Price { get; set; }
+        [Required]
 
         public bool IsAvailable { get; set; }
 
+        [Required]
         public int CategoryId { get; set; }
 
         [JsonIgnore]
 
-        public virtual Category Category { get; set; }
+        public virtual Category? Category { get; set; }
     }
 }
